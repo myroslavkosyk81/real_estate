a5d89f4b4f15d6624a0dc1b79328271a10e649f0
myroslavkosyk81 <myroslav.kosyk@ukr.net>
2023-10-26 15:50:53 +0300

    Created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Signin from './pages/SignUp';
+import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
   return <BrowserRouter>
+  <Header />
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<Signin />} />
+      <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/about" element={<About />} />
       <Route path="/profile" element={<Profile />} />
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Signout() {
+export default function SignIn() {
   return (
     <div>
-      
+      signin
     </div>
   )
 }
