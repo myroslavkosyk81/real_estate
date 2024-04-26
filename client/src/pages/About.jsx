6126dbc5a517863bf27d72a8b64ab69a6053afc9
myroslavkosyk81 <myroslav.kosyk@ukr.net>
2024-04-27 01:01:26 +0300

    fixed problem with header position

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -9,14 +9,14 @@ export default function About() {
     <div className='py-20 px-4 max-w-6xl mx-auto h-[calc(100vh_-_15vh)]'>
       
       <h1 className='text-3xl font-bold mb-4 text-slate-800'>{t('about.about1')} BestDomivka</h1>
-      <p className='mb-4 text-slate-700'>
+      <p className='mb-4 text-slate-700 pb-10 text-xl'>
       {t('about.mission')}
       </p>
-      <p className='mb-4 text-slate-700'>
+      <p className='mb-4 text-slate-700 pb-10 text-xl'>
       {t('about.platform1')}
-      <p><br />
+      <p className=''><br />
       {t('about.platform2')}</p>
-      <p><br />
+      <p className=' '><br />
         {t('about.bsb')}</p>
 
         
